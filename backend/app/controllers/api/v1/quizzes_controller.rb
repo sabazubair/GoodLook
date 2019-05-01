1e1308cedaf633950d012385c19d268e3c49cf18
Maliha Hossain <malu.hoss@gmail.com>
2019-05-01 19:24:27 -0400

    added quizzes route and quizzes controller to send required information as json

---
+++ backend/app/controllers/api/v1/quizzes_controller.rb
@@ -0,0 +1,36 @@
+module Api::V1
+  class QuizzesController < ApplicationController
+    def new
+      @quiz = Question.all.sort_by {|question| question.order }.map do |question|
+        { id: question.id,
+          text: question.text,
+          choices: question.question_choices
+        }
+      end
+
+      render json: @quiz
+
+
+
+      # [
+      #   { id: 1,
+      #     text: "abc",
+      #     choices: [
+      #       { id: 1,
+      #         text: "hello",
+      #         style_id: 10,
+      #         image: ""
+      #       },
+      #       { id: 2,
+      #         text: "there",
+      #         style_id: 11,
+      #         image: ""
+      #       }
+      #     ]
+      #   }
+      # ]
+      # render json: @questions
+    end
+
+  end
+end
