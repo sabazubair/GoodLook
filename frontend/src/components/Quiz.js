2019fcc2ccd68878a03e3e440b12a3f188b7e1a8
Sarah Farah <farahsaa@gmail.com>
2019-05-01 23:02:45 -0400

    added proxy to package.json to redirect requests to backend, set state with response from axios request, specified port for react-app to run on in package.json

--- frontend/src/components/Quiz.js
+++ frontend/src/components/Quiz.js
@@ -1,7 +1,24 @@
 import React, {Component} from 'react'
-
+import axios from "axios"
 export default class Quiz extends Component {
-  render () {
-    return <h1>Quiz page</h1>
+  constructor() {
+    super();
+    this.state = {
+      questions:[]
     }
+  }
+
+  componentDidMount() {
+    axios("/api/v1/quizzes/new")
+    .then(response => {
+      console.log(response.data)
+       this.setState({questions:response.data})
+    })
+    .catch(error => console.log(error))
+  
+  }
+
+  render(){
+    return (<h1>Quiz page</h1>)
+  }
 }
