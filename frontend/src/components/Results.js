1c344dfd2aab95975d81e7dded585e794a35ac6f
Maliha Hossain <malu.hoss@gmail.com>
2019-05-02 19:57:27 -0400

    created results react component

---
+++ frontend/src/components/Results.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+export default class Result extends Component {
+
+  render(){
+    return (
+      <div>
+      <h2>Results</h2>
+
+    </div>
+    );
+  }
+}
