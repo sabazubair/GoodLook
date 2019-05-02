777bc0f967eca050a2d1519b158727dd0315508e
Sarah Farah <farahsaa@gmail.com>
2019-05-02 14:59:18 -0400

    displayed choices and pictures

--- frontend/src/components/Quiz.js
+++ frontend/src/components/Quiz.js
@@ -1,25 +1,23 @@
 import React, {Component} from 'react'
 import axios from "axios"
 
-class Question extends Component{
-  constructor() {
-    super();
-  }
+// class Question extends Component{
+//   constructor() {
+//     super();
+//   }
 
-  render(){
-   let {question, choices} = this.props
+//   render(){
+//    let {question, choices} = this.props
 
-    return  (<div>
-      <p>{question}</p>
+//     return  (<div>
+//       <p>{question}</p>
 
-    </div>
-     )
+//     </div>
+//      )
 
-  }
+//   }
 
-}
-
-
+// }
 
 export default class Quiz extends Component {
   constructor() {
@@ -41,10 +39,19 @@ export default class Quiz extends Component {
 
   render(){
     let question = this.state.questions.map((question)=>{
-      const {choices,text} = question
-      
-        return (<Question  question={text} /> )
-     
+        let choices = question.choices.map((choice)=>{
+          return (
+          <div> 
+            <img width={100} height={100} src= {choice.image}/> 
+            <p>{choice.text}</p>
+          </div>)
+        })
+
+        return (
+        <div> 
+          <p>{question.text}</p>
+          <p>{choices}</p>
+        </div>)
     })
     
     return <div>{question}</div>
