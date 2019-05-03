dc76780aac23bb839ec11f60c79fa606ef4edaf8
Saba Zubair <szubair7865@gmail.com>
2019-05-03 15:44:02 -0400

    changed outfit seed

--- backend/app/controllers/api/v1/outfits_controller.rb
+++ backend/app/controllers/api/v1/outfits_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::OutfitsController < ApplicationController
   def show
     @result = Result.find_by user_id: 1
-    @style = Style.find_by id: @result.style_id
-    @outfit = Outfit.all.find_by style_id: @style.id
+    @outfit = Outfit.all.find_by style_id: @result.style_id
 
     render json: @outfit
   end
