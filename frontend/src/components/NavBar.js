16e7861caa2b8a7b165b55f3d6a0bb3e685b45b3
Saba Zubair <szubair7865@gmail.com>
2019-05-04 19:40:47 -0400

    styling navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -7,13 +7,10 @@ export default class NavBar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <Link className="navbar-brand" to="/">Good Look</Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
           <div className="collapse navbar-collapse" id="navbarText">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <Link className="nav-link" to="/about">About <span class="sr-only">(current)</span></Link>
+                <Link className="nav-link" to="/about">About<span class="sr-only">(current)</span></Link>
               </li>
               <li class="nav-item">
                 <Link className="nav-link" to="/meettheteam">Meet the Team</Link>
