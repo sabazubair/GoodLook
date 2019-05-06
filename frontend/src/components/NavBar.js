5078fda2ef6b79997ccb161e4ede094c106c81dc
Maliha Hossain <malu.hoss@gmail.com>
2019-05-05 22:18:14 -0400

    changed class to className

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -10,7 +10,7 @@ export default class NavBar extends Component {
           <div className="collapse navbar-collapse" id="navbarText">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <Link className="nav-link" to="/about">About<span class="sr-only">(current)</span></Link>
+                <Link className="nav-link" to="/about">About<span className="sr-only">(current)</span></Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/meettheteam">Meet the Team</Link>
