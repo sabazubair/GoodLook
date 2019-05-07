a897ef7405986ed1c112c0e358721d317432f196
Maliha Hossain <malu.hoss@gmail.com>
2019-05-06 21:24:28 -0400

    trying to have button to redirect to store link

--- frontend/src/components/AfterQuiz/Recommendations/SingleRecommendation.js
+++ frontend/src/components/AfterQuiz/Recommendations/SingleRecommendation.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {Component} from 'react';
+import { Link } from "react-router-dom";
+import { Redirect } from 'react-router-dom';
 
-export default ({outfit, handleClick}) => {
-  return (
-    <div>
-      <img id={outfit.id} style={{width:'20%'}} src={outfit.image} />
-      <i className="fas fa-heart" outfit={outfit} id={outfit.id} style={{color: outfit.selected ? 'red' : 'black'}} onClick={handleClick}></i>
-      <p>{outfit.text}</p>
-    </div>
-  )
+export default class SingleRecommendation extends Component {
+  redirectToLink = () => {
+    return <Redirect to= {this.props.outfit.link}/>
+  }
+
+  render() {
+    return (
+      <div>
+        <img id={this.props.outfit.id} style={{width:'20%'}} src={this.props.outfit.image} />
+        <i className="fas fa-heart" outfit={this.props.outfit} id={this.props.outfit.id} style={{color: this.props.outfit.selected ? 'red' : 'black'}} onClick={this.props.handleClick}></i>
+        <p>{this.props.outfit.text}</p>
+        <button onClick={this.redirectToLink}>Shop this look</button>
+      </div>
+    )
+  }
+
 }
+
 
