665213f853605ec016a6b4814bc74a798619e66e
Saba Zubair <szubair7865@gmail.com>
2019-05-07 22:35:52 -0400

    created profile page

--- frontend/src/components/AfterQuiz/AfterQuiz.js
+++ frontend/src/components/AfterQuiz/AfterQuiz.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./NavBar.js";
 import Recommendations from "./Recommendations/Recommendations.js";
 import TestingWardrobe from "./TestingWardrobe.js";
+import Profile from "./Profile.js"
 
 export default class AppRouter extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class AppRouter extends Component {
         <NavBar />
         <Route path="/afterquiz/recommendations" component={Recommendations} />
         <Route path="/afterquiz/wardrobe" component={TestingWardrobe} />
+        <Route path="/afterquiz/profile" component={Profile} />
       </Router>
     );
   }
